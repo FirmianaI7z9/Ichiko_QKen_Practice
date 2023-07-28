447b261cb81ccd0fe5667a23665fa209b9654b92
Firmiana-Unity <122654548+FirmianaI7z9@users.noreply.github.com>
2023-07-28 09:46:19 +0900

    Update display

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ async function init() {
     document.getElementById('user_data_name').textContent = decodeURI(json.id);
     document.getElementById('user_data_name').style = 'color: #' + getColor(decodeURI(json.id), json.cor);
     document.getElementById('user_data_detail').innerHTML = 
-      '累計解答問題数：<span style="font-size: 110%; font-weight: bold;">' + json.ans + '問</span> / '
-      + '累計正解問題数：<span style="font-size: 110%; font-weight: bold;">' + json.cor + '問</span> / '
-      + '正解率：<span style="font-size: 110%; font-weight: bold;">' + (Math.floor(json.rate * 100000) / 1000).toFixed(3) + '%</span>';
+      '<nobr>累計解答問題数：<span style="font-size: 110%; font-weight: bold;">' + json.ans + '問</span> / <wbr>'
+      + '累計正解問題数：<span style="font-size: 110%; font-weight: bold;">' + json.cor + '問</span> / <wbr>'
+      + '正解率：<span style="font-size: 110%; font-weight: bold;">' + (Math.floor(json.rate * 100000) / 1000).toFixed(3) + '%</span></nobr>';
   } else {
     document.getElementById('user_data_name').textContent = 'ユーザ未登録';
   }
